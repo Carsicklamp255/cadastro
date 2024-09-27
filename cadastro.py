55fbd19705b01fad79367a3ed76392df2ac0083c
Carsicklamp255 <160789738+Carsicklamp255@users.noreply.github.com>
2024-09-27 00:24:55 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -9,79 +9,94 @@ file_exist = os.path.exists(file_name)
 if not file_exist:
   with open(file_name, mode= 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=['nome_completo', 'idade', 'CPF',
-    'endereço','telefone', 'email'])
+    'endereco','telefone', 'email','produto'])
     writer.writeheader()
+    print(f'o arquivo {file_name} foi criado')
 
-print(f'o arquivo {file_name} foi criado')
-resposta = input("Você deseja se cadastrar um usuario, consuntar um cadastro ou excluir?").strip()
-
-def cadastro():  
-    nome_completo = input('digite seu nome completo:').title().isalpha()
-    while not nome_completo.title().isalpha():
-      print("digite apenas letras por favor")
-      nome_completo = input("digite seu nome completo: ")
-    idade = input('digite sua idade: ')
-    while not idade.isdigit():
-      print("a idade deve ser infomado apenas com digitos!")
+resposta = input("Você deseja se cadastrar um usuario, consultar um cadastro, excluir ou sair do sistema? ")
+while not resposta == 'sair':
+  def cadastro():  
+      nome_completo = input('digite seu nome completo: ').strip().title()
+      while not nome_completo.replace(" ", "").isalpha():
+        print("digite apenas letras por favor")
+        nome_completo = input("digite seu nome completo: ").strip().title()
       idade = input('digite sua idade: ')
-    CPF = input('digite seu CPF:')
-    while not CPF.isdigit():
-      print('apenas os números devem ser informados: ')
+      while not idade.isdigit():
+        print("a idade deve ser infomado apenas com digitos!")
+        idade = input('digite sua idade: ')
       CPF = input('digite seu CPF: ')
-    endereco = input('digite seu endereço:').istitle()
-    telefone = input('digite seu número de telefone:').isnumeric
-    email = input('digite seu email:').strip()
+      while not CPF.isdigit():
+        print('Digite apenas os digitos do CPF devem ser informados: ')
+        CPF = input('digite seu CPF: ')
+      endereco = input('digite seu endereço: ')
+      telefone = input('digite seu número de telefone: ')
+      while not telefone.isdigit():
+        print("digite apenas o número de seu telefone para contato")
+        telefone = input('digite seu número corretamete: ')
+      email = input('digite seu email: ').strip()
+      produto = input("digite o nome do produto a ser entregue ou serviço que foi pedido: ").strip()
+      with open('cadastro.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([nome_completo, idade, CPF, endereco, telefone, email, produto])
 
-    with open('cadastro.csv', 'a', newline='') as file:
-      writer = csv.writer(file)
-      writer.writerow([nome_completo, idade, CPF, endereco, telefone, email])
+        print('cadastro realizado com sucesso')
 
-      print('cadastro realizado com sucesso')
+  def listar_cadastro():
+      with open('cadastro.csv', 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)  # Ler o cabeçalho
+        for row in reader:
+            for i in range(len(header)):
+                print(f"{header[i]}: {row[i]}")
+            print("-" * 40)
+            break
+          
+  def consulta_cadastro():
+      nome_completo = input('Digite o nome do cadastrado que deseja consultar: ').strip().title()
+      with open('cadastro.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+          if row[0].strip().title() == nome_completo:
+            print(row)
+            break
+        else:
+          print('Cadastro não encontrado')
 
-def listar_cadastro():
+  def excluir_cadastro():
+    nome_completo = input('Digite o nome do cadastro que deseja excluir: ').strip().title()
     with open('cadastro.csv', 'r') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        print(row)
+        reader = csv.reader(file)
+        rows = list(reader)
+        for row in rows:
+          if row[0] == nome_completo.strip().title():
+            rows.remove(row)
+            break
 
+        else:
+          print('Cadastro não encontrado')
 
-def consulta_cadastro():
-    nome_completo = input('Digite o nome dp cadastrp que deseja consultar: ').strip()
-    with open('cadastro.csv', 'r') as file:
-      reader = csv.reader(file)
-      for row in reader:
-        if row[0] == nome_completo:
-          print(row)
-          break
-      else:
-        print('Cadastro não encontrado')
+    with open('cadastro.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
 
+        print('Cadastro excluido com sucesso')
 
+  def saida_sistema():
+    print("Saindo do sistema...")
 
-def excluir_cadastro():
-  nome_completo = input('Digite o nome do cadastro que deseja excluir: ').strip()
-  with open('cadastro.csv', 'r') as file:
-      reader = csv.reader(file)
-      rows = list(reader)
-      for row in rows:
-        if row[0] == nome_completo:
-          rows.remove(row)
-          break
-
-      else:
-        print('Cadastro não encontrado')
-
-  with open('cadastro.csv', 'w', newline='') as file:
-      writer = csv.writer(file)
-      writer.writerows(rows)
-
-      print('Cadastro excluido com sucesso')
-
-if resposta.lower() == 'cadastrar':
-  cadastro()
-
-if resposta.lower() == 'consultar':
-  consulta_cadastro()
-
-if resposta.lower() == 'excluir':
-  excluir_cadastro()
+  if resposta.lower() == 'cadastrar':
+    cadastro()
+    input("Você deseja se cadastrar um usuario, consultar um cadastro, excluir ou sair do sistema? ")
+  if resposta.lower() == 'consultar':
+    consulta_cadastro()
+    input("Você deseja se cadastrar um usuario, consultar um cadastro, excluir ou sair do sistema? ")
+  if resposta.lower() == 'excluir':
+    excluir_cadastro()
+    input("Você deseja se cadastrar um usuario, consultar um cadastro, excluir, sair do sistema ou ver a lista toda? ")
+  if resposta.lower() == 'sair':
+    saida_sistema()
+  if resposta.lower() == 'ver':
+    listar_cadastro()
+  elif resposta.lower() not in ('cadastrar', 'consultar', 'excluir', 'sair', 'ver'):
+    print('digite apenas as palavrar cadastrar, consultar, excluir, sair e ver!')
+    input("Você deseja se cadastrar um usuario, consultar um cadastro, excluir ou sair do sistema? ")
